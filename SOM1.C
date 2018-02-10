be7ab552cbd901e46cdedf3280562be22d566abf
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-02-10 21:18:58 +0530

    Update SOM1.C

--- SOM1.C
+++ SOM1.C
@@ -4,16 +4,13 @@ void main()
 int a;
 printf("Enter a value");
 scanf("%d",&a);
+if(1<=a<=100000)
+{
 if(a>0)
-{
 printf("THE ENTERED VALUE IS POSITIVE");
-}
 else if(a<0)
-{
 printf("THE ENTERED VALUE IS NEGATIVE");
-}
 else
-{
 printf("THE ENTERED VALUE IS ZERO");
 }
 }
